58679ef950689a94192221548786b54ece96c8f1
Duncan Dean <duncangleeddean@gmail.com>
2022-12-16 11:25:12 +0200

    Add download progress indicators

--- utils/construct.py
+++ utils/construct.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import urllib.request
-import shutil
 import os
 import os.path
 from datetime import datetime
@@ -65,6 +64,24 @@ FILES = {
     "ripe-26.gz": "https://data.ris.ripe.net/rrc26/%Y.%m/bview.%Y%m%d.0000.gz",
 }
 
+def download_dump(src, dst, callback, file_size):
+    downloaded_mib = 0
+    block_size = max(4096, file_size // 1000)
+    download_size_mib = file_size / 1024 / 1024
+
+    while True:
+        buf = src.read(block_size)
+        if not buf:
+            break
+        dst.write(buf)
+        downloaded_mib += len(buf) / 1024 / 1024
+        callback(downloaded_mib, download_size_mib)
+
+def display_progress(downloaded_mib, download_size_mib):
+    print('\r    {:.2f} / {:.2f} MiB --- {:.1f}%'.format(
+        downloaded_mib, download_size_mib,
+        (downloaded_mib / download_size_mib) * 100), end='')
+
 def construct(date):
     DIRNAME = "data-%s/" % date
     if not os.path.exists(DIRNAME):
@@ -79,7 +96,9 @@ def construct(date):
                 os.remove(fullpath + ".part")
             try:
                 with urllib.request.urlopen(eurl) as response, open(fullpath + ".part", "wb") as out_file:
-                    shutil.copyfileobj(response, out_file)
+                    file_size = int(response.getheader('content-length'))
+                    download_dump(response, out_file, display_progress, file_size);
+                    print()
                 os.rename(fullpath + ".part", fullpath)
             except OSError as err:
                 print("Failed to download %s: %s" % (filename, err))
