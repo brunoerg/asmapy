a952b5718de87a8633af828632088c8b5218a098
Bruno Garcia <brunoely.gc@gmail.com>
2023-01-03 17:55:08 -0300

    feat: allow use the `to-human-readable` passing an array of path

--- utils/parse.py
+++ utils/parse.py
@@ -14,7 +14,21 @@ def parse(dirs=[], all_asn=False, single_output=True):
                     for entry in bgp:
                         if not isinstance(entry.body, TableDumpV2):
                             continue
+    for dir in dirs:
+        for entry_file in os.scandir(dir):
+            if entry_file.is_file():
+                with BGPDump(entry_file.path) as bgp:
+                    print(f"Processing {entry_file.name}...")
+                    for entry in bgp:
+                        if not isinstance(entry.body, TableDumpV2):
+                            continue
 
+                        prefix = '%s/%d' % (entry.body.prefix, entry.body.prefixLength)
+                        if not all_asn:
+                            list_ASN = set([
+                                route.attr.asPath.split()[-1]
+                                for route
+                                in entry.body.routeEntries])
                         prefix = '%s/%d' % (entry.body.prefix, entry.body.prefixLength)
                         if not all_asn:
                             list_ASN = set([
@@ -29,10 +43,10 @@ def parse(dirs=[], all_asn=False, single_output=True):
                                 route.attr.asPath
                                 for route
                                 in entry.body.routeEntries])
-
+                            
                             for item in list(list_ASN):
                                 routes.append(f'{prefix}|{item}\n')
-
+                    
                     if not single_output:
                         if not os.path.exists(f"paths-{dir}"):
                             os.mkdir(f"paths-{dir}")
